7339cea3b4e2ca0a75854a07b77d87438f5c7c3d
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-20 08:50:00 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -261,6 +261,33 @@ const Form = {
     Form.date.value = "";
   },
 
+   metas() {
+    let incomeMetas = Transaction.incomes() / 100;
+    let boxMetaToday = document.querySelector("#metaToday");
+  
+    if (incomeMetas > 0 && incomeMetas < 10) {
+      boxMetaToday.classList.add("metaTodayUm");
+    } else if (incomeMetas > 11 && incomeMetas < 20) {
+      boxMetaToday.classList.add("metaTodayDois");
+    } else if (incomeMetas > 21 && incomeMetas < 30) {
+      boxMetaToday.classList.add("metaTodayTres");
+    } else if (incomeMetas > 31 && incomeMetas < 40) {
+      boxMetaToday.classList.add("metaTodayQuatro");
+    } else if (incomeMetas > 41 && incomeMetas < 50) {
+      boxMetaToday.classList.add("metaTodayCinco");
+    } else if (incomeMetas > 51 && incomeMetas <= 60) {
+      boxMetaToday.classList.add("metaTodaySeis");
+    } else if (incomeMetas > 61 && incomeMetas < 70) {
+      boxMetaToday.classList.add("metaTodaySete");
+    } else if (incomeMetas > 71 && incomeMetas < 80) {
+      boxMetaToday.classList.add("metaTodayOito");
+    } else if (incomeMetas > 81 && incomeMetas < 90) {
+      boxMetaToday.classList.add("metaTodayNove");
+    } else {
+      boxMetaToday.classList.add("metaTodayDez");
+    }
+  },
+
   submit(event) {
     event.preventDefault();
 
@@ -274,6 +301,8 @@ const Form = {
       // apagar os dados do formulário
       Form.clearFields();
       // modal feche
+      Form.metas();
+
       Modal.close();
       // atualizar a aplicação // Já temos um App.reload() no add
     } catch (error) {
@@ -312,39 +341,5 @@ App.init();
 // console.log(presentDayIncome);
 // }
 
-let incomeMetas = Transaction.incomes() / 100;
-let boxMetaToday = document.querySelector("#metaToday");
-
-if (incomeMetas > 0 && incomeMetas < 10) {
-  boxMetaToday.classList.add("metaTodayUm");
-  alert("Meta 1");
-} else if (incomeMetas > 11 && incomeMetas < 20) {
-  boxMetaToday.classList.add("metaTodayDois");
-  alert("Meta 2");
-} else if (incomeMetas > 21 && incomeMetas < 30) {
-  boxMetaToday.classList.add("metaTodayTres");
-  alert("Meta 3");
-} else if (incomeMetas > 31 && incomeMetas < 40) {
-  boxMetaToday.classList.add("metaTodayQuatro");
-  alert("Meta 4");
-} else if (incomeMetas > 41 && incomeMetas < 50) {
-  boxMetaToday.classList.add("metaTodayCinco");
-  alert("Meta 5");
-} else if (incomeMetas > 51 && incomeMetas <= 60) {
-  boxMetaToday.classList.add("metaTodaySeis");
-  alert("Meta 6");
-} else if (incomeMetas > 61 && incomeMetas < 70) {
-  boxMetaToday.classList.add("metaTodaySete");
-  alert("Meta 7");
-} else if (incomeMetas > 71 && incomeMetas < 80) {
-  boxMetaToday.classList.add("metaTodayOito");
-  alert("Meta 8");
-} else if (incomeMetas > 81 && incomeMetas < 90) {
-  boxMetaToday.classList.add("metaTodayNove");
-  alert("Meta 9");
-} else {
-  boxMetaToday.classList.add("metaTodayDez");
-  alert("Meta 10");
-}
 
 console.log(incomeMetas);
