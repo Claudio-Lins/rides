cfdc6358816b072feeb6d30c53e98a7c25b3d707
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-23 19:44:07 +0100

    add week number

--- scripts/week.js
+++ scripts/week.js
@@ -1,14 +1,15 @@
-//CurrentWeek
-// Date.prototype.getWeek = function () {
-//   var onejan = new Date(this.getFullYear(), 0, 1);
-//   var today = new Date(this.getFullYear(), this.getMonth(), this.getDate());
-//   var dayOfYear = (today - onejan + 86400000) / 86400000;
-//   return Math.ceil(dayOfYear / 7);
-// };
+let CurrentWeek
+Date.prototype.getWeek = function () {
+  var onejan = new Date(this.getFullYear(), 0, 1);
+  var today = new Date(this.getFullYear(), this.getMonth(), this.getDate());
+  var dayOfYear = (today - onejan + 86400000) / 86400000;
+  return Math.ceil(dayOfYear / 7);
+};
 
-// var today = new Date();
-// var currentWeekNumber = today.getWeek();
-// console.log('Semana: ' + currentWeekNumber);
+var today = new Date();
+var currentWeekNumber = today.getWeek()-1;
+document.querySelector('#currentWeek').innerHTML = currentWeekNumber;
+console.log('Semana: ' + currentWeekNumber);
 
 // Start week
 // SEGUNDA FEIRA
